747f4ba1c2857a156af0f4518ff30b2b96eb6b20
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-07-18 23:21:50 +0300

    add icons

--- app/src/main/java/com/fighter/donutsapp/ui/screens/details/Details.kt
+++ app/src/main/java/com/fighter/donutsapp/ui/screens/details/Details.kt
@@ -1,2 +1,20 @@
 package com.fighter.donutsapp.ui.screens.details
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DetailsScreen(){
+    DetailsContent()
+}
+
+@Composable
+fun DetailsContent(){
+
+}
+
+@Preview
+@Composable
+fun PreviewDetails() {
+    DetailsScreen()
+}
