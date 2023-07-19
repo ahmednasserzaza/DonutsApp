2f1e59711bc5cabee79702cf011c85760acf6b41
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-07-19 18:04:28 +0300

    implement getDonuts fun in DetailsViewModel

--- app/src/main/java/com/fighter/donutsapp/ui/screens/details/DetailsViewModel.kt
+++ app/src/main/java/com/fighter/donutsapp/ui/screens/details/DetailsViewModel.kt
@@ -2,11 +2,13 @@ package com.fighter.donutsapp.ui.screens.details
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.fighter.donutsapp.R
 import com.fighter.donutsapp.ui.navigation.DonutDetailArgs
 import com.fighter.donutsapp.ui.screens.home.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,7 +26,64 @@ class DetailsViewModel @Inject constructor(
     }
 
     private fun getDonutDetails(donutId: Int) {
-
+        when(donutId){
+            0 -> _state.update {
+                it.copy(
+                    image = R.drawable.offer_one,
+                    name = "Strawberry Wheel",
+                    description = "These Baked Strawberry Donuts are filled with fresh strawberries...",
+                    price = "$16"
+                )
+            }
+            1 -> _state.update {
+                it.copy(
+                    image = R.drawable.offer_two,
+                    name = "Chocolate Glaze",
+                    description = "Moist and fluffy baked chocolate donuts full of chocolate flavor.",
+                    price = "$18"
+                )
+            }
+            2 -> _state.update {
+                it.copy(
+                    image = R.drawable.offer_one,
+                    name = "Craze Donut",
+                    description = "The Red Velvet donut is a visually stunning treat with its vibrant red hue and a rich cocoa flavor. It's often topped with a cream cheese frosting, adding a tangy and creamy element to the sweet donut.",
+                    price = "$25"
+                )
+            }
+            3 -> _state.update {
+                it.copy(
+                    image = R.drawable.donut_1,
+                    name = "Chocolate Cherry",
+                    description = "These Baked Strawberry Donuts are filled with fresh strawberries...",
+                    price = "$16"
+                )
+            }
+            4 -> _state.update {
+                it.copy(
+                    image = R.drawable.donut_2,
+                    name = "Chocolate Glaze",
+                    description = "Moist and fluffy baked chocolate donuts full of chocolate flavor.",
+                    price = "$18"
+                )
+            }
+            5 -> _state.update {
+                it.copy(
+                    image = R.drawable.donut_3,
+                    name = "Craze Donut",
+                    description = "The Red Velvet donut is a visually stunning treat with its vibrant red hue and a rich cocoa flavor. It's often topped with a cream cheese frosting, adding a tangy and creamy element to the sweet donut.",
+                    price = "$25"
+                )
+            }
+            else -> _state.update {
+                it.copy(
+                    image = R.drawable.donut_1,
+                    name = "Chocolate Cherry",
+                    description = "The Red Velvet donut is a visually stunning treat with its vibrant red hue and a rich cocoa flavor. It's often topped with a cream cheese frosting, adding a tangy and creamy element to the sweet donut.",
+                    price = "$25"
+                )
+            }
+        }
     }
 
 }
